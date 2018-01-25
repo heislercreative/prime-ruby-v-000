f907b724e1b2833de1360882b73c21ffb7fb0eea
Brennan Heisler <learn@heislercreative.com>
2018-01-25 04:26:50 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,8 @@ def prime?(integer)
   if integer < 2
     false
   else
-    (2..integer).to_a.none? do |i|
-      integer % i == 0
+    (2..integer-1).to_a.all? do |i|
+      integer % i != 0
     end
   end
 end
