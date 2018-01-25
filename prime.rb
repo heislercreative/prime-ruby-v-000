cbcef9fd98a5306db06f296b6e2f05a877337be7
Brennan Heisler <learn@heislercreative.com>
2018-01-25 04:19:49 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,7 @@
 def prime?(integer)
-
-  i = 0
-  while i < integer.length
-    if integer[i] % 2 == 0 && array[i] != 2
-      false
-    elsif integer[i] % 3 == 0 && array[i] != 3
-      false
-    elsif integer[i] % 5 == 0 && array[i] != 5
-      false
-    else
-      true
-    end
-    i += 1
+  if integer < 0 || integer == 0 || integer == 1
+    false
+  else
+    
   end
 end
